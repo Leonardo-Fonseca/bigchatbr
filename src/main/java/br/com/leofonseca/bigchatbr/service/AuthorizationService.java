4c455a98b935c25095e0f408664c9a7438c2e2cd
Leonardo Fonseca <leonardosfonseca@icloud.com>
2025-04-21 15:14:56 -0300

    feat: refact on AuthorizationService.java, adding logs with @Slf4j.

--- src/main/java/br/com/leofonseca/bigchatbr/service/AuthorizationService.java
+++ src/main/java/br/com/leofonseca/bigchatbr/service/AuthorizationService.java
@@ -2,6 +2,7 @@ package br.com.leofonseca.bigchatbr.service;
 
 import br.com.leofonseca.bigchatbr.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,13 +12,25 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AuthorizationService implements UserDetailsService {
     private final UserRepository repository;
 
     @Override
     public UserDetails loadUserByUsername(String documentId) throws UsernameNotFoundException {
-        return Optional.ofNullable(repository.findByDocumentId(documentId))
-                .orElseThrow(() ->
-                        new UsernameNotFoundException("Usuário não encontrado: " + documentId));
+        log.debug("Buscando usuário com documentId: {}", documentId);
+
+        UserDetails userDetails = Optional.ofNullable(repository.findByDocumentId(documentId))
+                .orElseThrow(() -> {
+
+                    log.error("Usuário não encontrado: {}", documentId);
+
+                    return new UsernameNotFoundException("Usuário não encontrado: " + documentId);
+
+                });
+
+        log.info("Usuário encontrado: {}", documentId);
+
+        return userDetails;
     }
 }
--- src/main/java/br/com/leofonseca/bigchatbr/service/ClientService.java
+++ src/main/java/br/com/leofonseca/bigchatbr/service/ClientService.java
@@ -9,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @Service

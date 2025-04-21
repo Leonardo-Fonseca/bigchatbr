f66b862a83092f32a1653f526abc28f7fc4d13f3
Leonardo Fonseca <leonardosfonseca@icloud.com>
2025-04-21 15:25:38 -0300

    feat: refact on UserService.java, adding logs with @Slf4j.

--- src/main/java/br/com/leofonseca/bigchatbr/service/UserService.java
+++ src/main/java/br/com/leofonseca/bigchatbr/service/UserService.java
@@ -8,15 +8,18 @@ import br.com.leofonseca.bigchatbr.domain.user.UserRole;
 import br.com.leofonseca.bigchatbr.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class UserService {
     private final UserRepository userRepository;
 
     public User createUserFromClient(ClientCreateRequestDTO data) {
+        log.debug("Criando usuário a partir dos dados do cliente: {}", data.documentId());
 
         User user = new User();
         user.setName(data.name());
@@ -25,14 +28,22 @@ public class UserService {
         user.setIsActive(data.isActive());
         user.setUserRole(UserRole.CLIENTE);
         user.setPassword(data.password());
+        User savedUser = userRepository.save(user);
 
-        return userRepository.save(user);
+        log.info("Usuário criado com sucesso: {}", savedUser.getDocumentId());
+
+        return savedUser;
     }
 
     public void updateUserPassword(Long id, String password) {
+        log.debug("Atualizando senha do usuário com ID: {}", id);
+
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Usuario não encontrado"));
+
         user.setPassword(password);
+
+        log.info("Senha atualizada para o usuário com ID: {}", id);
     }
 
 }
